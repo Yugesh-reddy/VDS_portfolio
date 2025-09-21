89e6d342d97218000c6814ee87a5aa24ba28b72e
Yugesh Sappidi <yugeshreddysappidi@gmail.com>
2025-09-21 16:55:58 -0500

    Clean up unnecessary files and fix Eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 const now = String(Date.now())
 
 module.exports = function (eleventyConfig) {
+  // Ignore directories and files that shouldn't be processed
+  eleventyConfig.ignores.add("docs")
+  eleventyConfig.ignores.add("_site")
+  eleventyConfig.ignores.add("node_modules")
+  eleventyConfig.ignores.add("README.md")
+
   eleventyConfig.addWatchTarget('./styles/tailwind.config.js')
   eleventyConfig.addWatchTarget('./styles/tailwind.css')
 
--- Makefile
+++
@@ -1,3 +0,0 @@
-update:
-	scp -r -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedAlgorithms=+ssh-rsa -oProxyJump=bertvm.cs.uic.edu _site/* facwebs.cs.uic.edu:WWW
-
